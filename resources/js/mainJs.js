c8a396f695825ec3696a8f669076325d5028c8bc
JoKangYeon <145143819+JoKangYeon@users.noreply.github.com>
2023-11-30 19:26:42 +0900

    Delete resources directory

--- resources/js/mainJs.js
+++
@@ -1,9 +0,0 @@
-let allPA = document.querySelectorAll(".products>a")
-
-allPA.forEach(el => {el.addEventListener("click", () => {
-    alert("로그인 후 이용가능합니다.")
-})})
-
-document.querySelector("#cart").addEventListener("click", () => {
-    alert("로그인 후 이용가능합니다.")
-})
